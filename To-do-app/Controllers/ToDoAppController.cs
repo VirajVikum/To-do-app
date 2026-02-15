253f997a07f9190d57a0711add0df26360710911
Viraj Vikum <virajempire2001@gmail.com>
2026-02-15 12:19:54 +0530

    updated appsettings.json to add connection string

---
+++ To-do-app/Controllers/ToDoAppController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace To_do_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoAppController : ControllerBase
+    {
+    }
+}
